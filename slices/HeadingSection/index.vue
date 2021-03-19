15cef9e70f73e39c5f3ded3de6966d157785fd73
Jack Gregory <jack@platformplatform.com>
2021-03-19 12:46:50 +0000

    Adjust padding

--- slices/HeadingSection/index.vue
+++ slices/HeadingSection/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="section py-20 lg:py-20 flex justify-center px-8">
+  <section class="section py-20 lg:pt-40 lg:pb-40 flex justify-center px-8">
     <prismic-rich-text
         :field="slice.primary.heading"
         class="font-serif text-center text-3xl lg:text-6xl lg:leading-tight lg:w-2/3"
