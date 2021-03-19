31c224f7e8d49e7223e5e2fa69cf1e2ea91b502f
Jack Gregory <jack@platformplatform.com>
2021-03-19 18:45:41 +0000

    Layout tweaks

--- layouts/error.vue
+++ layouts/error.vue
@@ -3,8 +3,8 @@
     <!-- Vue tag to add header component -->
     <header-prismic/>
     <div class="w-3/5 lg:w-2/5 mx-auto text-center text-brown pt-24 lg:pt-48">
-      <h1 class="font-serif mb-5 text-7xl">Page not found</h1>
-      <p class="mb-3 text-xl">Sorry we were unable to find the page you are looking for.</p>
+      <h1 class="font-serif mb-5 text-5xl lg:text-7xl">Page not found</h1>
+      <p class="mb-3 lg:text-xl">Sorry we were unable to find the page you are looking for.</p>
     </div>
   </div>
 </template>
--- slices/ContentGrid/index.vue
+++ slices/ContentGrid/index.vue
@@ -7,8 +7,10 @@
       />
     </header>
     <div class="flex flex-wrap lg:w-11/12 mx-auto">
-      <div v-for="(item, i) in slice.items" :key="`slice-item-${i}`" class="lg:w-1/2 px-2 mb-12 text-center">    
-        <prismic-image :field="item.image" class="inline-block" />
+      <div v-for="(item, i) in slice.items" :key="`slice-item-${i}`" class="lg:w-1/2 px-2 mb-12 text-center">
+        <prismic-link :field="item.ctaLink"> 
+          <prismic-image :field="item.image" class="inline-block" />
+        </prismic-link>
         <prismic-rich-text :field="item.heading" class="font-serif text-2xl lg:text-4xl mb-8 mt-8 lg:mt-12 xl:w-4/5 mx-auto leading-snug" />
         <div class="my-4 lg:text-xl" v-if="$prismic.asText(item.ctaText) !==''">
           <prismic-link 
