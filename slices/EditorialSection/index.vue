5c26326b3eb96a8b01b3f7726aed4bcecb007239
Jack Gregory <jack@platformplatform.com>
2021-03-21 13:57:17 +0000

    Adds ID to image

--- slices/EditorialSection/index.vue
+++ slices/EditorialSection/index.vue
@@ -4,39 +4,53 @@
     :style="sectionStyles"
   >
     <div class="lg:w-10/12 mx-auto px-8">
-      <header class="mb-8 lg:px-24 lg:mb-24 lg:mt-2" v-if="$prismic.asText(slice.primary.heading) !==''">
+      <header
+        class="mb-8 lg:px-24 lg:mb-24 lg:mt-2"
+        v-if="$prismic.asText(slice.primary.heading) !== ''"
+      >
         <prismic-rich-text
           :field="slice.primary.heading"
           class="font-serif text-center text-3xl lg:text-6xl lg:leading-tight"
         />
       </header>
       <div class="lg:flex items-center lg:-mx-12">
-          <div class="lg:w-1/2 lg:px-12" :class="!slice.primary.image.url ? 'pb-0': 'pb-10'">
-            <!-- <prismic-image v-if="slice.primary.image.url" :field="slice.primary.image"/> -->
-            <img v-if="slice.primary.image.url" :data-src="slice.primary.image.url"  class="lazyload" :alt="slice.primary.image.alt" />
+        <div
+          class="lg:w-1/2 lg:px-12"
+          :class="!slice.primary.image.url ? 'pb-0' : 'pb-10'"
+        >
+          <!-- <prismic-image v-if="slice.primary.image.url" :field="slice.primary.image"/> -->
+          <img v-if="slice.primary.image.url"
+          :data-src="slice.primary.image.url" class="lazyload"
+          :alt="slice.primary.image.alt"
+          :id="slice.primary.image.alt | kebabCase" />
+        </div>
+        <div class="lg:w-1/2 lg:px-12 max-w-prose">
+          <prismic-rich-text
+            v-if="$prismic.asText(slice.primary.leadText) !== ''"
+            :field="slice.primary.leadText"
+            class="text-xl lg:text-3xl font-serif mb-8"
+          />
+          <prismic-rich-text
+            :field="slice.primary.text"
+            class="rich-text links lg:text-2xl py-3 font-medium"
+          />
+          <prismic-rich-text
+            v-if="$prismic.asText(slice.primary.altText) !== ''"
+            :field="slice.primary.altText"
+            class="lg:text-3xl font-serif mb-8"
+          />
+          <div
+            class="my-4 lg:text-xl"
+            v-if="$prismic.asText(slice.primary.ctaText) !== ''"
+          >
+            <prismic-link
+              :field="slice.primary.ctaLink"
+              class="border-b border-current pb-1 hover:text-magenta transition-colors font-bold"
+            >
+              {{ $prismic.asText(slice.primary.ctaText) }}
+            </prismic-link>
           </div>
-          <div class="lg:w-1/2 lg:px-12 max-w-prose">
-            <prismic-rich-text
-                v-if="$prismic.asText(slice.primary.leadText) !==''"
-                :field="slice.primary.leadText" class="text-xl lg:text-3xl font-serif mb-8"
-              />
-              <prismic-rich-text
-                :field="slice.primary.text"
-                class="rich-text links lg:text-2xl py-3 font-medium"
-              />
-              <prismic-rich-text
-                v-if="$prismic.asText(slice.primary.altText) !==''"
-                :field="slice.primary.altText" class="lg:text-3xl font-serif mb-8"
-              />
-              <div class="my-4 lg:text-xl" v-if="$prismic.asText(slice.primary.ctaText) !==''">
-                <prismic-link 
-                  :field="slice.primary.ctaLink"
-                  class="border-b border-current pb-1 hover:text-magenta transition-colors font-bold"
-                >
-                  {{ $prismic.asText(slice.primary.ctaText) }}
-                </prismic-link>
-              </div>
-          </div>
+        </div>
       </div>
     </div>
   </section>
@@ -49,21 +63,29 @@ export default {
       type: Object,
       required: true,
       default() {
-        return {}
+        return {};
       },
     },
+  },
+  
+  filters: {
+    kebabCase: function (value) {
+      if (!value) return ''
+      value = value.toString()
+      return value.replace(/\W+/g, "-").toLowerCase()
+    }
   },
 
   computed: {
     sectionStyles() {
       if (!this.slice.primary.backgroundImage.url) {
-        return
+        return;
       }
 
       return {
-        backgroundImage: `url(${this.slice.primary.backgroundImage.url})`
-      }
-    }
-  }
-}
+        backgroundImage: `url(${this.slice.primary.backgroundImage.url})`,
+      };
+    },
+  },
+};
 </script>
