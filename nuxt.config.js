3975adf0aa19427b57f724d05030843d970ebf7d
Jack Gregory <jack@platformplatform.com>
2021-08-17 09:48:30 +0100

    Add post nested

--- nuxt.config.js
+++ nuxt.config.js
@@ -140,6 +140,11 @@ export default {
   // Build Configuration (https://go.nuxtjs.dev/config-build)
   build: {
     transpile: ["vue-slicezone", "nuxt-sm"],
+    postcss: {
+      plugins: {
+        "postcss-nested": {},
+      },
+    },
   },
 
   storybook: {
